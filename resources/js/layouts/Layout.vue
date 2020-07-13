043209b85d6087abbefb95f347c45357affc9f4c
zjl <zjalen@163.com>
2020-07-13 21:39:33 +0800

    feat(gototop): 增加快速到顶部的按钮

--- resources/js/layouts/Layout.vue
+++ resources/js/layouts/Layout.vue
@@ -1,38 +1,38 @@
 <template>
-    <v-app id="inspire">
-        <left-menu ></left-menu>
-        <top-header></top-header>
-        <v-main>
-            <v-container fluid>
-                <router-view></router-view>
-            </v-container>
-        </v-main>
-        <bottom-footer></bottom-footer>
-        <snack-bar-toast></snack-bar-toast>
-        <my-dialog></my-dialog>
-    </v-app>
+  <v-app id="inspire">
+    <left-menu></left-menu>
+    <top-header></top-header>
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+    <go-to-top></go-to-top>
+    <bottom-footer></bottom-footer>
+    <snack-bar-toast></snack-bar-toast>
+    <my-dialog></my-dialog>
+  </v-app>
 </template>
 
 <script>
-  import LeftMenu from './LeftMenu'
-  import TopHeader from './TopHeader'
-  import SnackBarToast from '../components/SnackBarToast'
-  import MyDialog from '../components/MyDialog'
-  import BottomFooter from './BottomFooter'
-  export default {
-    name: 'Layout',
-    components: { BottomFooter, MyDialog, SnackBarToast, LeftMenu, TopHeader },
-    data: () => ({
+import LeftMenu from './LeftMenu'
+import TopHeader from './TopHeader'
+import SnackBarToast from '../components/SnackBarToast'
+import MyDialog from '../components/MyDialog'
+import BottomFooter from './BottomFooter'
+import GoToTop from './GoToTop'
 
-    }),
-    methods: {
-    }
-  }
+export default {
+  name: 'Layout',
+  components: { GoToTop, BottomFooter, MyDialog, SnackBarToast, LeftMenu, TopHeader },
+  data: () => ({}),
+  methods: {},
+}
 </script>
 
 <style lang="scss" scoped>
-    .theme--dark.v-application {
-        background: #000 !important;
-        color: #FFFFFF;
-    }
+  .theme--dark.v-application {
+    background: #000 !important;
+    color: #FFFFFF;
+  }
 </style>
