f2970ecae5c6955d4546c37a08a851d0fc05bb1c
zjl <zjalen@163.com>
2020-07-21 16:30:36 +0800

    fix(comment): 修复后台 api comment with 条件

--- app/Http/Controllers/Admin/CommentsController.php
+++ app/Http/Controllers/Admin/CommentsController.php
@@ -15,4 +15,15 @@ class CommentsController extends AdminBaseController
         $this->setBuilder($builder);
         $this->setModel(new Comment());
     }
+
+    public function index()
+    {
+        $filters = request()->query();
+
+        $query = $this->buildQuery($this->getBuilder(), $filters, $this->getAllowSearchColumns(), $this->getAllowShowColumns());
+        $total_count = $query->count();
+        $query = $this->buildQuery($this->getBuilder(), $filters, $this->getAllowSearchColumns(), $this->getAllowShowColumns(), true);
+        $items = $query->with('article')->get();
+        return $this->success(['items' => $items, 'total_count' => $total_count]);
+    }
 }
